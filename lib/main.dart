bfbf081619dd07a07b3838785bda401d86430c51
tamaki <hbk2002320@gmail.com>
2021-08-12 00:08:45 +0900

    :speakles: logincheacker

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_demo_firebase/roomList_page.dart';
 import 'package:flutter_demo_firebase/login_page.dart';
 import 'package:flutter_demo_firebase/userState.dart';
 import 'package:provider/provider.dart';
@@ -26,8 +28,22 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: LoginPage(),
+        home: _LoginCheck(),
       ),
     );
+  }
+}
+
+class _LoginCheck extends StatelessWidget {
+  const _LoginCheck({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ログイン状態に応じて、画面を切り替える
+    // ユーザー情報を受け取る
+    final UserState userState = Provider.of<UserState>(context);
+    final User? user = userState.user;
+
+    return user != null ? RoomListPage() : LoginPage();
   }
 }
