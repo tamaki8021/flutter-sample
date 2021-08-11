e00a7533cce5035843dcc5a72709c0881893ccd8
tamaki <hbk2002320@gmail.com>
2021-08-11 23:46:06 +0900

    feat: add authstatechange

--- lib/userState.dart
+++ lib/userState.dart
@@ -1,10 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 // 更新可能なデータ
 class UserState extends ChangeNotifier {
-  User? user;
+  UserState() : super() {
+    _authStateChangesSubscription?.cancel();
+    _authStateChangesSubscription =
+        FirebaseAuth.instance.authStateChanges().listen(
+      (data) {
+        if (data == null) {
+          user = data;
+        } else {
+          user = data;
+        }
+      },
+    );
+  }
 
+  StreamSubscription<User?>? _authStateChangesSubscription;
+
+  User? user;
   void setUser(User newUser) {
     user = newUser;
     notifyListeners();
